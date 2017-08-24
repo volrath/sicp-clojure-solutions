07971023d88f7aea30e1aa5f2dd9a28955c86272
Daniel Barreto <dbarreto@talpor.com>
2017-08-24 21:17:23 +0200

    Minor corrections to 2.1 exercises

--- src/sicp/chapter2/exercise_06.clj
+++ src/sicp/chapter2/exercise_06.clj
@@ -2,16 +2,16 @@
 
 (def zero (fn [f] (fn [x] x)))
 
-(defn add-1 [n]
+(defn succ [n]
   (fn [f] (fn [x] (f ((n f) x)))))
 
-(def one (fn [f] (fn [x] (f x))))
-(def two (fn [f] (fn [x] (f (f x)))))
+(def one  (fn [f] (fn [x] (f x))))
+(def two  (fn [f] (fn [x] (f (f x)))))
 
 (defn plus [n1 n2]
   (fn [f]
     (fn [x]
       ((n1 f) ((n2 f) x)))))
 
-(assert (= (((plus one two) inc) 0) 3))
 ((zero inc) 0)
+(assert (= (((plus one two) inc) 0) 3))
--- src/sicp/chapter2/exercise_10.clj
+++ src/sicp/chapter2/exercise_10.clj
@@ -1,7 +1,10 @@
 (ns sicp.chapter2.exercise-10
-  (:require [sicp.chapter2.exercise-07 :as e07]))
+  (:require [clojure.test :refer [is]]
+            [sicp.chapter2.exercise-07 :as e07]))
 
 (defn div-interval [i1 i2]
-  {:pre [(not (zero? (e07/lower-bound i2)))
-         (not (zero? (e07/upper-bound i2)))]}
+  {:pre [(pos? (* (e07/lower-bound i2) (e07/upper-bound i2)))]}
   (e07/div-interval i1 i2))
+
+(is (thrown? Exception (div-interval (e07/make-interval 0 4)
+                                     (e07/make-interval -2 2))))
